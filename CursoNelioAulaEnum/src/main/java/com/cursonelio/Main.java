1b52bfae8f0579aff0b908e7c03cae3377f40750
Geovane Clemente <geo.zn13@gmail.com>
2024-03-31 12:47:12 -0300

    adicionando projeto de aula de enum

---
+++ CursoNelioAulaEnum/src/main/java/com/cursonelio/Main.java
@@ -0,0 +1,17 @@
+package com.cursonelio;
+
+import com.cursonelio.entities.Order;
+import com.cursonelio.entities.enums.OrderStatus;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Order order = new Order(1080, new Date(), OrderStatus.PROCESSING);
+        System.out.println(order);
+        OrderStatus os1 = OrderStatus.DELIVERED;
+        OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+        System.out.println(os1);
+        System.out.println(os2);
+    }
+}
